9abd419d51ce982b3124551674e0ba0ab06598cf
jamesbriant <james@briant.co.uk>
2023-04-12 09:48:17 +0100

    Create analysis directory

---
+++ analysis/create_npy.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import numpy as np
+from datetime import datetime, date, timedelta
+from typing import Dict
+
+from script_variables import *
+
+
+def read_grd(filename) -> np.ndarray:
+    nv3d = 4
+    nv2d = 2
+    f = np.fromfile(filename, dtype=np.float32)
+    shape = (nlon,nlat,nv3d*nlev+nv2d)
+    data = np.reshape(f, shape, order="F")
+    data = data.astype(np.float64)
+    return data
+
+def loop_through_grd(folder, filenames, n) -> Dict[str, np.ndarray]:
+    u = np.zeros((nlon, nlat, nlev, n)) # wind u
+    v = np.zeros_like(u)                # wind v
+    t = np.zeros_like(u)                # temperature
+    q = np.zeros_like(u)                # specific humidity
+    precip = np.zeros((nlon, nlat, n))  # precipitation
+    p = np.zeros_like(precip)           # surface pressure
+
+    i = 0
+    for date in filenames:
+        for file in os.scandir(folder):
+            f = os.path.join(folder, file)
+            # checking if it is a file and if correct date
+            if os.path.isfile(f) and date in f and "_fluxes.grd" not in f:
+                data = read_grd(f)
+
+                u[..., i] = data[..., 0:8]
+                v[..., i] = data[..., 8:16]
+                t[..., i] = data[..., 16:24]
+                q[..., i] = data[..., 24:32]
+                precip[..., i] = data[..., 32]
+                p[..., i] = data[..., 33]
+
+                i += 1
+
+    return {
+            'u': u, 
+            'v': v, 
+            't': t,
+            'q': q, 
+            'precip': precip,
+            'q': p,
+        }
+
+
+def read_flx(filename) -> np.ndarray:
+    f = np.fromfile(filename, dtype=np.float64)
+    shape = (nlon, nlat, nrec)
+    data = np.reshape(f, shape, order="F")
+    # data = data.astype(np.float64)
+    return data
+
+def loop_through_flx(folder, filenames, n) -> Dict[str, np.ndarray]:
+    cloudc  = np.zeros((nlon, nlat, n))
+    clstr   = np.zeros_like(cloudc)
+    precnv  = np.zeros_like(cloudc)
+    precls  = np.zeros_like(cloudc)
+    tsr     = np.zeros_like(cloudc)
+    olr     = np.zeros_like(cloudc)
+
+    i = 0
+    for date in filenames:
+        for file in os.scandir(folder):
+            f = os.path.join(folder, file)
+            # checking if it is a file and if correct date
+            if os.path.isfile(f) and date in f and "_fluxes.grd" in f:
+                data = read_flx(f, nlon, nlat)
+                
+                cloudc[..., i]  = data[..., 0]
+                clstr[..., i]   = data[..., 1]
+                precnv[..., i]  = data[..., 2]
+                precls[..., i]  = data[..., 3]
+                tsr[..., i]     = data[..., 4]
+                olr[..., i]     = data[..., 5]
+
+                i += 1
+
+    return {
+            'cloudc': cloudc,
+            'clstr': clstr, 
+            'precnv': precnv,
+            'precls': precls,
+            'tsr': tsr,
+            'olr': olr,
+        }
+
+
+#######################################################
+# Find the file names
+
+# Dimensions of the grid
+nlon = 96
+nlat = 48
+nlev = 8
+nrec = 6
+
+# Prepare the December, Janurary and February data
+winter = ["12", "01", "02"]
+summer = ["06", "07", "08"]
+
+# Time increments
+delta = timedelta(hours=6)
+
+# Initial Date
+idate = "1982010100"
+SPEEDY_DATE_FORMAT = "%Y%m%d%H"
+
+# Set up array of files
+filenames_winter = []
+filenames_summer = []
+
+# # Populate the allowed dates
+while idate != "1992010100":
+    if idate[4:6] in winter:
+        filenames_winter.append(idate)
+    elif idate[4:6] in summer:
+        filenames_summer.append(idate)
+    newdate = datetime.strptime(idate, SPEEDY_DATE_FORMAT) + delta
+    idate = newdate.strftime(SPEEDY_DATE_FORMAT)
+
+
+n_winter = len(filenames_winter)
+n_summer = len(filenames_summer)
+
+
+
+
+###########################################################
+# Create the npy files
+
+ 
+# if analysis directory doesn't exist, make the directory.
+if not os.path.isdir(os.path.join(analysis_root, "analysis")):
+    os.mkdir(os.path.join(analysis_root, "analysis"))
+
+#################### NATURE ####################
+if NATURE:
+    ################################
+    ######## WINTER
+
+    # non-fluxes
+    output = loop_through_grd(
+            os.path.join(SPEEDY_nature_root, "DATA", "nature"), 
+            filenames_winter, 
+            n_winter
+        )
+    for varname, array in output:
+        filename = os.path.join(analysis_root, "analysis", f"{varname}_DJF_nature.npy")
+        np.save(filename, array)
+
+    #fluxes
+    output = loop_through_flx(
+            os.path.join(SPEEDY_nature_root, "DATA", "nature"), 
+            filenames_winter, 
+            n_winter
+        )
+    for varname, array in output:
+        filename = os.path.join(analysis_root, "analysis", f"{varname}_DJF_nature.npy")
+        np.save(filename, array)
+    
+    ################################
+    ######## SUMMER
+
+    # non-fluxes
+    output = loop_through_grd(
+            os.path.join(SPEEDY_nature_root, "DATA", "nature"), 
+            filenames_summer, 
+            n_summer
+        )
+    for varname, array in output:
+        filename = os.path.join(analysis_root, "analysis", f"{varname}_JJA_nature.npy")
+        np.save(filename, array)
+
+    # fluxes
+    output = loop_through_flx(
+            os.path.join(SPEEDY_nature_root, "analysis", "DATA", "nature"), 
+            filenames_summer, 
+            n_summer
+        )
+    for varname, array in output:
+        filename = os.path.join(analysis_root, "analysis", f"{varname}_JJA_nature.npy")
+        np.save(filename, array)
+
+
+
+
+#################### FUSION ####################
+if FUSION:
+    ################################
+    ######## WINTER
+
+    # non-fluxes
+    output = loop_through_grd(
+            SPEEDY_data_read_root, 
+            filenames_winter, 
+            n_winter
+        )
+    for varname, array in output:
+        filename = os.path.join(analysis_root, "analysis", f"{varname}_DJF_fusion.npy")
+        np.save(filename, array)
+
+    # fluxes
+    output = loop_through_flx(
+            SPEEDY_data_read_root, 
+            filenames_winter, 
+            n_winter
+        )
+    for varname, array in output:
+        filename = os.path.join(analysis_root, "analysis", f"{varname}_DJF_fusion.npy")
+        np.save(filename, array)
+    
+    ################################
+    ######## SUMMER
+
+    # non-fluxes
+    output = loop_through_grd(
+            SPEEDY_data_read_root, 
+            filenames_summer, 
+            n_summer
+        )
+    for varname, array in output:
+        filename = os.path.join(analysis_root, "analysis", f"{varname}_JJA_fusion.npy")
+        np.save(filename, array)
+
+    # fluxes
+    output = loop_through_flx(
+            SPEEDY_data_read_root, 
+            filenames_summer, 
+            n_summer
+        )
+    for varname, array in output:
+        filename = os.path.join(analysis_root, "analysis", f"{varname}_JJA_fusion.npy")
+        np.save(filename, array)
---
+++ analysis/script_variables_template.py
@@ -0,0 +1,13 @@
+#Directory locations and script constants
+
+#COPY & RENAME THIS FILE TO script_variables.py AND EDIT FOR YOUR SETUP
+
+#Different for each user
+analysis_root =         '' #Path to analysis .npy files after running SPEEDY
+
+SPEEDY_nature_root =    '' #Path to speedy_nature directory
+SPEEDY_data_read_root = '' #Location .grd files from GP predictions ready for SPEEDY simulation
+
+# Flags
+FUSION = True
+NATURE = True
