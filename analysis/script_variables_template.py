0d3f1edb91569c7d0c3ae34573807ea2778a46e9
jamesbriant <james@briant.co.uk>
2023-08-02 16:33:33 +0100

    Add orography variance for SPEEDY

--- analysis/script_variables_template.py
+++ analysis/script_variables_template.py
@@ -5,10 +5,14 @@
 GP_name = '' # Do NOT include the .pkl file extention
 
 #Different for each user
+gp_directory_root =     '' #Path to your gp.pkl files
+pngs_root =             '' #Path to where the pngs will be saved to
 analysis_root =         '' #Path to analysis .npy files after running SPEEDY
 
+oro_var_data_file = '' #Path to orography variance data file
+
 SPEEDY_fusion_data_root = '' #Location .grd files from GP predictions ready for SPEEDY fusion simulation
-SPEEDY_nature_root =    '' #Path to speedy_nature directory
+SPEEDY_nature_root =      '' #Path to speedy_nature directory
 
 # Flags
 FUSION = True
--- speedy_fusion/src/script_variables_template.py
+++ speedy_fusion/src/script_variables_template.py
@@ -10,6 +10,8 @@ pngs_root =             '' #Path to where the pngs will be saved to
 gp_directory_root =     '' #Path to your gp.pkl files
 analysis_root =         '' #Path to analysis .npy files after running SPEEDY
 
+oro_var_data_file = '' #Path to orography variance data file
+
 SPEEDY_fusion_root =    '' #Path to speedy_fusion directory
 SPEEDY_fusion_data_root = '' #Location .grd files from GP predictions ready for SPEEDY fusion simulation
 SPEEDY_nature_root =    '' #Path to speedy_nature directory
@@ -22,4 +24,4 @@ subregion_count = 4
 subregion_resolution = 224
 
 # Flags
-TRAIN_GP = True #Change to False to split up workflow and use pre-trained GP
+TRAIN_GP = False #Change to False to split up workflow and use pre-trained GP
