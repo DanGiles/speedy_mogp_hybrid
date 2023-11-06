4081089d7a60871bdc9a8a2b1d3bf4b978ce2fb9
jamesbriant <james@briant.co.uk>
2023-11-06 12:13:38 +0000

    Add pcolormesh plots

--- analysis/plot_lifted_index.py
+++ analysis/plot_lifted_index.py
@@ -9,8 +9,9 @@ from typing import List
 
 from script_variables import *
 
-analysis_root = '/Users/jamesbriant/Documents/Projects/ml_climate_fusion/data/analysis' #override for local compute, otherwise comment out
-pngs_root = '/Users/jamesbriant/Documents/Projects/ml_climate_fusion/pngs' #override for local compute, otherwise comment out
+# SPEEDY_root = '/Users/jamesbriant/Documents/Projects/ml_climate_fusion/speedy' #override for local compute, otherwise comment out
+# analysis_root = '/Users/jamesbriant/Documents/Projects/ml_climate_fusion/data/analysis' #override for local compute, otherwise comment out
+# pngs_root = '/Users/jamesbriant/Documents/Projects/ml_climate_fusion/pngs' #override for local compute, otherwise comment out
 
 level_925 = 0
 level_850 = 1
@@ -95,6 +96,141 @@ def plot_scatter(ax, lon_index, lat_index, field_data, title, divnorm, heatmap=N
     return heatmap
 
 
+def plot_scatter_wrapper(counted_LI, title):
+    fig, ax = plt.subplots(
+        2, 1, 
+        figsize=(8, 8),
+        subplot_kw={'projection': ccrs.PlateCarree()}
+    )
+
+    vmin=min(min(counted_LI['india']), min(counted_LI['africa']), -1)
+    vmax=max(max(counted_LI['india']), max(counted_LI['africa']), 1)
+    divnorm = mpl.colors.TwoSlopeNorm(vmin=vmin, vcenter=0, vmax=vmax)
+
+    heatmap = plot_scatter(
+        ax[0],
+        lon_index_india,
+        lat_index_india,
+        counted_LI['india'],
+        "India",
+        divnorm
+    )
+    heatmap = plot_scatter(
+        ax[1],
+        lon_index_africa,
+        lat_index_africa,
+        counted_LI['africa'],
+        "Africa",
+        divnorm,
+        heatmap=heatmap
+    )
+
+    fig.colorbar(heatmap, ax=ax)
+    fig.suptitle(f'Difference in No. of Lifted Index values < {title} \n (Hybrid - SPEEDY); Season: {season}')
+
+    plt.savefig(
+        os.path.join(output_path, f'lifted_index_scatter_{season}_{title}.png')
+    )
+    plt.close()
+
+
+def read_const_grd(filename, nlon, nlat, var):
+    # Orography = 0
+    # Land/Sea Mask = 1
+    num = 5
+    f = np.fromfile(filename, dtype=np.float32)
+    shape = (nlon,nlat,num)
+    data = np.reshape(f, shape, order="F")
+    data = data.astype(np.float64)
+    return data[:,:,var]
+
+lsm = read_const_grd(os.path.join(SPEEDY_root, "model", "data/bc/t30/clim", "sfc.grd"), nlon, nlat, 1)
+lsm = np.flip(lsm.T, 0)
+
+
+def plot_pcolormesh_scatter(
+    ax, 
+    lon_index, 
+    lat_index, 
+    field_data, 
+    title, 
+    divnorm, 
+    extend=2
+):
+    lon_index_pcolormesh = lon_index[:]
+    lat_index_pcolormesh = lat_index[:]
+    for _ in range(extend):
+        lon_index_pcolormesh.insert(0, min(lon_index_pcolormesh)-1)
+        lat_index_pcolormesh.insert(0, max(lat_index_pcolormesh)+1)
+        lon_index_pcolormesh.append(max(lon_index_pcolormesh)+1)
+        lat_index_pcolormesh.append(min(lat_index_pcolormesh)-1)
+
+    a, b, _ = get_index_mesh(
+        [x + extend - lon_index[0] for x in lon_index], 
+        [x + extend - lat_index[-1] for x in lat_index]
+    )
+
+    # print(lon_index, lon_index_pcolormesh)
+    # print(lat_index, lat_index_pcolormesh)
+
+    heatmap_pcolormesh = ax.pcolormesh(
+        [x - lon_index_pcolormesh[0] for x in lon_index_pcolormesh],
+        [x - lat_index_pcolormesh[-1] for x in lat_index_pcolormesh][::-1],
+        lsm[lat_index_pcolormesh[::-1], :][:, lon_index_pcolormesh], 
+        shading='auto',
+        # cmap="Greys"
+    )
+
+    heatmap_scatter = ax.scatter(
+        a, b, 
+        c=field_data,
+        s=400,
+        edgecolors='k',
+        cmap='PiYG',
+        norm=divnorm
+    )
+    ax.set_aspect('auto')
+    ax.set_title(title)
+    return heatmap_pcolormesh, heatmap_scatter
+
+
+def plot_pcolormesh_scatter_wrapper(counted_LI, title):
+    fig, ax = plt.subplots(
+        2, 1,
+        figsize=(10, 8)
+    )
+
+    vmin=min(min(counted_LI['india']), min(counted_LI['africa']), -1)
+    vmax=max(max(counted_LI['india']), max(counted_LI['africa']), 1)
+    divnorm = mpl.colors.TwoSlopeNorm(vmin=vmin, vcenter=0, vmax=vmax)
+
+    plot_pcolormesh_scatter(
+        ax[0],
+        lon_index_india_points,
+        lat_index_india_points,
+        counted_LI['india'],
+        "India",
+        divnorm
+    )
+    heatmap_pcolormesh, heatmap_scatter = plot_pcolormesh_scatter(
+        ax[1],
+        lon_index_africa_points,
+        lat_index_africa_points,
+        counted_LI['africa'],
+        "Africa",
+        divnorm
+    )
+
+    fig.colorbar(heatmap_pcolormesh, ax=ax, location='left', label='Sea (0.0) -> Land (1.0) Mask')
+    fig.colorbar(heatmap_scatter, ax=ax)
+    fig.suptitle(f'Difference in No. of Lifted Index values < {title} \n (Hybrid - SPEEDY); Season: {season}')
+
+    plt.savefig(
+        os.path.join(output_path, f'lifted_index_scatter_pcolormesh_{season}_{title}.png')
+    )
+    plt.close()
+
+
 for season in seasons:
     print(season)
     counted_LI_m2 = {}
@@ -164,118 +300,13 @@ for season in seasons:
             )
             plt.close()
         
-
+    
     # MINUS 2
-    fig, ax = plt.subplots(
-        2, 1, 
-        figsize=(8, 8),
-        subplot_kw={'projection': ccrs.PlateCarree()}
-    )
-
-    vmin=min(min(counted_LI_m2['india']), min(counted_LI_m2['africa']), -1)
-    vmax=max(max(counted_LI_m2['india']), max(counted_LI_m2['africa']), 1)
-    divnorm = mpl.colors.TwoSlopeNorm(vmin=vmin, vcenter=0, vmax=vmax)
-
-    heatmap = plot_scatter(
-        ax[0],
-        lon_index_india,
-        lat_index_india,
-        counted_LI_m2['india'],
-        "India",
-        divnorm
-    )
-    heatmap = plot_scatter(
-        ax[1],
-        lon_index_africa,
-        lat_index_africa,
-        counted_LI_m2['africa'],
-        "Africa",
-        divnorm,
-        heatmap=heatmap
-    )
-
-    fig.colorbar(heatmap, ax=ax)
-    fig.suptitle(f'Difference in No. of Lifted Index values < -2 \n (Hybrid - SPEEDY); Season: {season}')
-
-    plt.savefig(
-        os.path.join(output_path, f'lifted_index_scatter_{season}_-2.png')
-    )
-    plt.close()
-
-
-
+    plot_scatter_wrapper(counted_LI_m2, "-2")
+    plot_pcolormesh_scatter_wrapper(counted_LI_m2, "-2")
     # MINUS 4
-    fig, ax = plt.subplots(
-        2, 1, 
-        figsize=(8, 8),
-        subplot_kw={'projection': ccrs.PlateCarree()}
-    )
-
-    vmin=min(min(counted_LI_m4['india']), min(counted_LI_m4['africa']), -1)
-    vmax=max(max(counted_LI_m4['india']), max(counted_LI_m4['africa']), 1)
-    divnorm = mpl.colors.TwoSlopeNorm(vmin=vmin, vcenter=0, vmax=vmax)
-
-    heatmap = plot_scatter(
-        ax[0],
-        lon_index_india,
-        lat_index_india,
-        counted_LI_m4['india'],
-        "India",
-        divnorm
-    )
-    heatmap = plot_scatter(
-        ax[1],
-        lon_index_africa,
-        lat_index_africa,
-        counted_LI_m4['africa'],
-        "Africa",
-        divnorm,
-        heatmap=heatmap
-    )
-
-    fig.colorbar(heatmap, ax=ax)
-    fig.suptitle(f'Difference in No. of Lifted Index values < -4 \n (Hybrid - SPEEDY); Season: {season}')
-
-    plt.savefig(
-        os.path.join(output_path, f'lifted_index_scatter_{season}_-4.png')
-    )
-    plt.close()
-
-
-
+    plot_scatter_wrapper(counted_LI_m4, "-4")
+    plot_pcolormesh_scatter_wrapper(counted_LI_m4, "-4")
     # MINUS 6
-    fig, ax = plt.subplots(
-        2, 1, 
-        figsize=(8, 8),
-        subplot_kw={'projection': ccrs.PlateCarree()}
-    )
-
-    vmin=min(min(counted_LI_m6['india']), min(counted_LI_m6['africa']), -1)
-    vmax=max(max(counted_LI_m6['india']), max(counted_LI_m6['africa']), 1)
-    divnorm = mpl.colors.TwoSlopeNorm(vmin=vmin, vcenter=0, vmax=vmax)
-
-    heatmap = plot_scatter(
-        ax[0],
-        lon_index_india,
-        lat_index_india,
-        counted_LI_m6['india'],
-        "India",
-        divnorm
-    )
-    heatmap = plot_scatter(
-        ax[1],
-        lon_index_africa,
-        lat_index_africa,
-        counted_LI_m6['africa'],
-        "Africa",
-        divnorm,
-        heatmap=heatmap
-    )
-
-    fig.colorbar(heatmap, ax=ax)
-    fig.suptitle(f'Difference in No. of Lifted Index values < -6 \n (Hybrid - SPEEDY); Season: {season}')
-
-    plt.savefig(
-        os.path.join(output_path, f'lifted_index_scatter_{season}_-6.png')
-    )
-    plt.close()
+    plot_scatter_wrapper(counted_LI_m6, "-6")
+    plot_pcolormesh_scatter_wrapper(counted_LI_m6, "-6")
