bacf5982c1e36ea017368b3ee2eb13410223bd8e
jamesbriant <james@briant.co.uk>
2024-05-10 12:21:37 +0100

    feat: Add script for plotting lifted index over land

--- analysis/lifted_index_from_nc.py
+++ analysis/lifted_index_from_nc.py
@@ -1,4 +1,6 @@
-# Run this script AFTER running create_npy.py
+# This script is VERY slow.
+# It takes about 30 hours to complete. This should probably be parallelized.
+# 200 chunks consumes about 8GB of RAM. Works well on my machine with 16GB RAM
 
 import os
 import numpy as np
@@ -10,19 +12,18 @@ from metpy.units import units
 
 from script_variables import *
 
+# SPEEDY_root = '/Users/jamesbriant/Documents/Projects/ml_climate_fusion/speedy' #override for local compute, otherwise comment out
+# analysis_root = '/Users/jamesbriant/Documents/Projects/ml_climate_fusion/data/analysis' #override for local compute, otherwise comment out
+
 level_925 = 0
 level_850 = 1
 level_700 = 2
 level_500 = 3
 
 
-# pressure_levels = [3000, 10000, 20000, 30000, 50000, 70000, 85000, 92500] # *units.Pa
-# sigma_levels = [0.95, 0.835, 0.685, 0.51, 0.34, 0.20, 0.095, 0.025]
-
-def get_LI(ps, t0, t1, t2, t3, q0, q1, q2, q3) -> np.ndarray:
+def get_LI(t0, t1, t2, t3, q0, q1, q2, q3) -> np.ndarray:
     """Calculates and returns the array of lifted index values"""
 
-    # press = [(sigma_levels[0]*ps*0.01), (sigma_levels[1]*ps*0.01), (sigma_levels[2]*ps*0.01), (sigma_levels[3]*ps*0.01)]
     #### Find entries the q values are non-negative
     id_q_500_pos = q3[:] >= 0
     id_q_700_pos = q2[:] >= 0
@@ -58,6 +59,16 @@ def get_LI(ps, t0, t1, t2, t3, q0, q1, q2, q3) -> np.ndarray:
             j += 1
     return LI
 
+def read_const_grd(filename, nlon, nlat, var):
+    # Orography = 0
+    # Land/Sea Mask = 1
+    num = 5
+    f = np.fromfile(filename, dtype=np.float32)
+    shape = (nlon,nlat,num)
+    data = np.reshape(f, shape, order="F")
+    data = data.astype(np.float64)
+    return data[:,:,var]
+
 
 output_dir = os.path.join(analysis_root, 'annual')
 
@@ -65,42 +76,51 @@ runs = ["HYBRID", "SPEEDY"]
 nlon = 96
 nlat = 48
 
-# List of points of interest
-poi = [(9, 23), (8, 24), (21, 26) , (21, 25) , (22, 25), 
-       (28, 24), (29, 24), (30, 24), (70, 26), (71, 26), 
-       (72, 26), (73, 26), (74, 26), (75, 26), (90, 23), 
-       (93, 25)]
+lsm = read_const_grd(os.path.join(SPEEDY_root, "model", "data/bc/t30/clim", "sfc.grd"), nlon, nlat, 1)
+lsm = np.flip(lsm.T, 0) # lsm.shape = (48, 96)
+poi = np.dstack(
+    np.meshgrid(
+        np.arange(0, nlon),
+        np.arange(0, nlat)
+    )
+).reshape(-1, 2)
+poi = poi[lsm.flatten() > 0.3, :] # Only keep cells with at least 30% land
 
 for run in runs:
-    print(run)
-    # Read in the NetCDFs
-    T = xr.load_dataset(os.path.join(output_dir, f'{run}_T.nc'))
-    Q = xr.load_dataset(os.path.join(output_dir, f'{run}_Q.nc'))
-    ps = xr.load_dataset(os.path.join(output_dir, f'{run}_ps.nc'))
-
-    ps_0 = ps['ps'].data
-    T_0 = T['T_0'].data
-    T_1 = T['T_1'].data
-    T_2 = T['T_2'].data
-    T_3 = T['T_3'].data
-
-    Q_0 = Q['Q_0'].data
-    Q_1 = Q['Q_1'].data
-    Q_2 = Q['Q_2'].data
-    Q_3 = Q['Q_3'].data
     # Loop through each location
-    for loc in poi:
-        lon, lat = loc[0], loc[1]
-        print(lon, lat)
-        output_array = np.zeros(ps_0.shape[0]) + 9999
-        LI_output = get_LI(ps_0[:, lon, lat], 
-                T_0[:, lon, lat], 
-                T_1[:, lon, lat], 
-                T_2[:, lon, lat], 
-                T_3[:, lon, lat],
-                Q_0[:, lon, lat], 
-                Q_1[:, lon, lat], 
-                Q_2[:, lon, lat], 
-                Q_3[:, lon, lat])
-        output_array[0:len(LI_output)] = LI_output
-        np.save(os.path.join(output_dir, 'global_lifted_index', f'{run}_LI_{lon}_{lat}.npy'), LI_output)
+    T = xr.load_dataset(os.path.join(output_dir, f'{run}_T.nc'), chunks={"timestamp": 200})
+    Q = xr.load_dataset(os.path.join(output_dir, f'{run}_Q.nc'), chunks={"timestamp": 200})
+    # ps = xr.load_dataset(os.path.join(output_dir, f'{run}_ps.nc'), chunks={"timestamp": 200})
+    
+    for i, (lon, lat) in enumerate(poi):
+        # lat, lon = loc[0], loc[1]
+        print(f'{run}: {i+1}/{len(poi)} - longitude: {lon}, latitude: {lat}')
+
+        # Read in the NetCDFs
+        T_fixed = T.isel(longitude=lon, latitude=lat)
+        Q_fixed = Q.isel(longitude=lon, latitude=lat)
+        # ps_fixed = ps.isel(longitude=lon, latitude=lat)
+
+        # ps_0 = ps_fixed['ps'].data
+        T_0 = T_fixed['T_0'].data
+        T_1 = T_fixed['T_1'].data
+        T_2 = T_fixed['T_2'].data
+        T_3 = T_fixed['T_3'].data
+
+        Q_0 = Q_fixed['Q_0'].data
+        Q_1 = Q_fixed['Q_1'].data
+        Q_2 = Q_fixed['Q_2'].data
+        Q_3 = Q_fixed['Q_3'].data
+
+        # output_array = np.zeros(ps_0.shape[0]) + 9999
+        try:
+            LI_output = get_LI(
+                # ps_0, 
+                T_0, T_1, T_2, T_3,
+                Q_0, Q_1, Q_2, Q_3
+            )
+            # output_array[0:len(LI_output)] = LI_output
+            np.save(os.path.join(output_dir, 'global_lifted_index', f'{run}_LI_{lon}_{lat}.npy'), LI_output)
+        except:
+            print(f'Error: {run} - lon: {lon}, lat: {lat}')
+            continue
---
+++ analysis/plot_lifted_index_land_nc.py
@@ -0,0 +1,119 @@
+import os
+import numpy as np
+from scipy.stats import skew, kurtosis
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import matplotlib as mpl
+import cartopy.feature as cfeature
+from typing import List
+
+from script_variables import *
+
+SPEEDY_root = '/Users/jamesbriant/Documents/Projects/ml_climate_fusion/speedy' #override for local compute, otherwise comment out
+analysis_root = '/Users/jamesbriant/Documents/Projects/ml_climate_fusion/data/analysis' #override for local compute, otherwise comment out
+pngs_root = '/Users/jamesbriant/Documents/Projects/ml_climate_fusion/pngs' #override for local compute, otherwise comment out
+
+
+def round_nearest_half(x):
+    return round(x * 2.0)/2
+
+nlon = 96
+nlat = 48
+nlev = 8
+
+# Set up the coordinate system
+lons = np.linspace(0, 360, nlon, endpoint=False) # endpoint=False to match SPEEDY
+# lat = np.linspace(-90, 90, nlat) # this does NOT match SPEEDY
+lat_vals = "-87.159 -83.479 -79.777 -76.070 -72.362 -68.652 -64.942 -61.232 -57.521 -53.810 -50.099 -46.389 -42.678 -38.967 -35.256 -31.545 -27.833 -24.122 -20.411 -16.700 -12.989  -9.278  -5.567  -1.856   1.856   5.567   9.278  12.989  16.700  20.411  24.122  27.833  31.545  35.256  38.967  42.678  46.389  50.099  53.810  57.521  61.232  64.942  68.652  72.362  76.070  79.777  83.479  87.159"
+lats = np.array([float(val) for val in lat_vals.split()])
+
+##### Loop through all the files #####
+speedy_m2 = np.zeros((nlon, nlat))
+hybrid_m2 = np.zeros((nlon, nlat))
+speedy_m4 = np.zeros((nlon, nlat))
+hybrid_m4 = np.zeros((nlon, nlat))
+speedy_n = np.zeros((nlon, nlat))
+hybrid_n = np.zeros((nlon, nlat))
+speedy_mean = np.zeros((nlon, nlat))
+hybrid_mean = np.zeros((nlon, nlat))
+
+folder = os.path.join(analysis_root, 'annual', 'global_lifted_index')
+for filename in os.listdir(folder):
+    if filename.startswith('SPEEDY_LI'):
+        lon = int(filename.split('_')[2])
+        lat = int(filename.split('_')[3].split('.')[0])
+        LI = np.load(os.path.join(folder, filename))
+        speedy_m2[lon, lat] = np.sum(LI < -2)
+        speedy_m4[lon, lat] = np.sum(LI < -4)
+        speedy_n[lon, lat] = len(LI)
+        speedy_mean[lon, lat] = np.mean(LI)
+    elif filename.startswith('HYBRID_LI'):
+        lon = int(filename.split('_')[2])
+        lat = int(filename.split('_')[3].split('.')[0])
+        LI = np.load(os.path.join(folder, filename))
+        hybrid_m2[lon, lat] = np.sum(LI < -2)
+        hybrid_m4[lon, lat] = np.sum(LI < -4)
+        hybrid_n[lon, lat] = len(LI)
+        hybrid_mean[lon, lat] = np.mean(LI)
+
+
+def read_const_grd(filename, nlon, nlat, var):
+    # Orography = 0
+    # Land/Sea Mask = 1
+    num = 5
+    f = np.fromfile(filename, dtype=np.float32)
+    shape = (nlon,nlat,num)
+    data = np.reshape(f, shape, order="F")
+    data = data.astype(np.float64)
+    return data[:,:,var]
+lsm = read_const_grd(os.path.join(SPEEDY_root, "model", "data/bc/t30/clim", "sfc.grd"), nlon, nlat, 1) # lsm.shape = (96, 48)
+lsm = np.flip(lsm.T, 0) # lsm.shape = (48, 96)
+
+
+def plot(ax, field_data, title):
+    vmin = np.nanmin(field_data)
+    vmax = np.nanmax(field_data)
+    vabsmax = max(abs(vmin), abs(vmax))
+    norm = mpl.colors.TwoSlopeNorm(vmin=-vabsmax, vcenter=0, vmax=vabsmax)
+
+    map = ax.pcolormesh(field_data, cmap='seismic', norm=norm)
+    fig.colorbar(map, ax=ax)
+    ax.set_title(f"Difference in {title}")
+    ax.set_xticks([0, 24, 48, 72, 96])
+    # ax.set_xticklabels([-180, -90, 0, 90, 180])
+    ax.set_xticklabels([0, 90, 180, 270, 360])
+    ax.set_yticks([0, 12, 24, 36, 48])
+    ax.set_yticklabels([-90, -45, 0, 45, 90])
+    ax.set_xlabel('Longitude')
+    ax.set_ylabel('Latitude')
+
+
+fig, axes = plt.subplots(
+    nrows=2,
+    ncols=2,
+    figsize=(15, 9),
+    # subplot_kw={'projection': ccrs.PlateCarree(central_longitude=180)}
+)
+fig.suptitle('Lifted Index over land\nHybrid minus SPEEDY')
+plot(
+    axes.flatten()[0], 
+    (hybrid_m2 - speedy_m2).T,
+    f"#(LI<2)"
+)
+plot(
+    axes.flatten()[1], 
+    (hybrid_m2/hybrid_n - speedy_m2/speedy_n).T,
+    f"% of points where (LI<2)"
+)
+plot(
+    axes.flatten()[2], 
+    (hybrid_m4 - speedy_m4).T,
+    f"#(LI<4)"
+)
+plot(
+    axes.flatten()[3], 
+    (hybrid_m4/hybrid_n - speedy_m4/speedy_n).T,
+    f"% of points where (LI<4)"
+)
+plt.savefig(os.path.join(pngs_root, 'annual', 'LI_diff.png'))
+# plt.show()
