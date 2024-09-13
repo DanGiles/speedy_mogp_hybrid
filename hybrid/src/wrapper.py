9f4e66572d1aa2cd675bc7b5edb769a6b243ef5f
jamesbriant <james@briant.co.uk>
2024-09-13 10:52:47 +0100

    Add comments

--- hybrid/src/wrapper.py
+++ hybrid/src/wrapper.py
@@ -11,46 +11,42 @@ import sys
 from mogp import *
 from script_variables import *
 
-def check_total_water(Q, Qs):
-    num = 0
-    rho = [1.1379, 1.0626, 0.908 , 0.6914, 0.4572]
-    for i in range(len(Q[:,0])):
-        water_content = np.sum(Q[i,:]*rho)
-        sample = np.sum(Qs[i,:]*rho)
-        diff = abs(sample - water_content)/water_content
-        if diff > 0.5:
-            Qs[i,:] = Q[i,:]
-            num +=1
-    print("Number of physically inconsistent profiles (total water content) %i"%num)
 
-    return Qs
+##### Physical Consistency Checks #####
+##### These functions are no longer used for a retained for posterity
 
+# def check_total_water(Q, Qs):
+#     num = 0
+#     rho = [1.1379, 1.0626, 0.908 , 0.6914, 0.4572]
+#     for i in range(len(Q[:,0])):
+#         water_content = np.sum(Q[i,:]*rho)
+#         sample = np.sum(Qs[i,:]*rho)
+#         diff = abs(sample - water_content)/water_content
+#         if diff > 0.5:
+#             Qs[i,:] = Q[i,:]
+#             num +=1
+#     print("Number of physically inconsistent profiles (total water content) %i"%num)
 
-def check_static_energy(Q, Qs, T, Ts):
-    num = 0
-    Cp = 1.005
-    Lv = 2260
-    for i in range(len(Q[:,0])):
-        static_energy = np.sum(Q[i,:]*Lv + Cp*T[i,:5])
-        sample_static_energy = np.sum(Qs[i,:]*Lv + Cp*Ts[i,:5])
-        diff = abs(sample_static_energy - static_energy)/static_energy
-        if diff > 0.05:
-            Ts[i,:] = T[i,:]
-            Qs[i,:] = Q[i,:]
-            num +=1
-    print("Number of physically inconsistent profiles (moist static energy) %i"%num)
-
-    return Ts, Qs
+#     return Qs
 
 
-def make_dir(path: str) -> None:
-    #do not empty directory if it doesn't exist!
-    if os.path.isdir(path):
-        import shutil
-        shutil.rmtree(path)
-    # make directory
-    os.mkdir(path)
-    
+# def check_static_energy(Q, Qs, T, Ts):
+#     num = 0
+#     Cp = 1.005
+#     Lv = 2260
+#     for i in range(len(Q[:,0])):
+#         static_energy = np.sum(Q[i,:]*Lv + Cp*T[i,:5])
+#         sample_static_energy = np.sum(Qs[i,:]*Lv + Cp*Ts[i,:5])
+#         diff = abs(sample_static_energy - static_energy)/static_energy
+#         if diff > 0.05:
+#             Ts[i,:] = T[i,:]
+#             Qs[i,:] = Q[i,:]
+#             num +=1
+#     print("Number of physically inconsistent profiles (moist static energy) %i"%num)
+
+#     return Ts, Qs
+
+
 
 def create_folders(output_folder):
     tmp = os.path.join(output_folder, "tmp")
@@ -62,7 +58,17 @@ def create_folders(output_folder):
     os.mkdir(tmp)
 
 
-def read_grd(filename, nlon, nlat, nlev):
+def read_grd(filename, nlon, nlat, nlev) -> np.ndarray:
+    """Reads in a SPEEDY .grd file and returns a numpy array of the data.
+    Use this function for reading in SPEEDY output files.
+
+    Args:
+        filename (str): The path to the .grd file
+        nlon (int): The number of longitudes
+        nlat (int): The number of latitudes
+        nlev (int): The number of levels
+    """
+
     nv3d = 4
     nv2d = 2
     f = np.fromfile(filename, dtype=np.float32)
@@ -71,7 +77,18 @@ def read_grd(filename, nlon, nlat, nlev):
     data = data.astype(np.float64)
     return data
 
-def read_const_grd(filename, nlon, nlat, var):
+
+def read_const_grd(filename, nlon, nlat, var) -> np.ndarray:
+    """Reads in a SPEEDY .grd file and returns a numpy array of the data.
+    Use this function for reading in the orography and land/sea mask.
+
+    Args:
+        filename (str): The path to the .grd file
+        nlon (int): The number of longitudes
+        nlat (int): The number of latitudes
+        var (int): The variable to extract
+    """
+    # argument var accepts the following values:
     # Orography = 0
     # Land/Sea Mask = 1
     num = 5
@@ -81,14 +98,23 @@ def read_const_grd(filename, nlon, nlat, var):
     data = data.astype(np.float64)
     return data[:,:,var]
 
-def speedy_update(SPEEDY, output_folder, YMDH, TYMDH):
+
+def speedy_update(SPEEDY, output_folder, YMDH, TYMDH) -> None:
+    """Execute next 6-hour SPEEDY forecast."""
     # Path to the bash script which carries out the forecast
     forecast = os.path.join(HYBRID_root, "src", "dafcst.sh")
     # Bash script call to speedy
     subprocess.check_call(str(forecast)+" %s %s %s %s" % (str(SPEEDY), str(output_folder), str(YMDH), str(TYMDH)),shell=True)
     return
 
-def write_fortran(filename, data):
+
+def write_fortran(filename, data) -> None:
+    """Writes a numpy array to a fortran binary file.
+    
+    Args:
+        filename (str): The path to the file to write to
+        data (np.ndarray): The numpy array to write to the file
+    """
     f=open(filename,'wb+')
     # data = data.flatten()
     data = data.astype(np.float32)
@@ -99,12 +125,22 @@ def write_fortran(filename, data):
 
 
 def step_datetime(idate, dtdate, SPEEDY_DATE_FORMAT, dt):
+    """Function to step the datetime by a given number of hours.
+    
+    Args:
+        idate (str): The initial date
+        dtdate (str): The forecast date
+        SPEEDY_DATE_FORMAT (str): The format of the date
+        dt (int): The number of hours to step the date by
+    """
     delta = timedelta(hours=dt)
     new_idate = datetime.strptime(idate, SPEEDY_DATE_FORMAT) + delta
     new_dtdate = datetime.strptime(dtdate, SPEEDY_DATE_FORMAT) + delta
     return new_idate.strftime(SPEEDY_DATE_FORMAT),new_dtdate.strftime(SPEEDY_DATE_FORMAT)
 
+
 def read_oro_var() -> np.ndarray:
+    """Reads in the orography variance data for SPEEDY."""
     oro_var_data = np.zeros((96, 48))
     oro_var_data_file = os.path.join(SPEEDY_root, "model","data","bc","t30","clim", "std_orog_for_speedy.dat")
     with open(oro_var_data_file) as f:
@@ -112,12 +148,27 @@ def read_oro_var() -> np.ndarray:
             oro_var_data[row_i, :] = np.fromstring(f.readline().strip(), dtype=float, sep=',')
     return oro_var_data
 
+
 def data_prep(data, oro, ls, nlon, nlat) -> np.ndarray:
-    T_mean = data[:,:,16:24]
-    Q_mean = data[:,:,24:29]
+    """Prepares the data for prediction with the MOGP model.
+    
+    Args:
+        data (np.ndarray): The SPEEDY output data
+        oro (np.ndarray): The orography data
+        ls (np.ndarray): The land/sea mask data
+        nlon (int): The number of longitudes
+        nlat (int): The number of latitudes
+    """
+    T_mean = data[:,:,16:24] # Temperature for all levels
+    Q_mean = data[:,:,24:29] # Specific Humidity for bottom 5 levels
+
     low_values_flags = Q_mean[:,:] < 1e-6  # Where values are low
     Q_mean[low_values_flags] = 1e-6
+
     # Version for gp_with_oro_var
+    # first axis is number of spatial grid points
+    # second axis is number of features
+    # surface level pressure, orography (mean and var), land/sea mask, temperature x8, specific humidity x5
     train = np.empty(((nlon*nlat),17), dtype = np.float64)
     train[:, 0] = data[:,:,32].flatten()
     train[:, 1] = oro[...,0].flatten()
@@ -128,6 +179,7 @@ def data_prep(data, oro, ls, nlon, nlat) -> np.ndarray:
 
     return train
 
+##### This function uses the physical consistency checks, but a no longer used. Retained for posterity.
 # def mogp_prediction_conserving(mogp_inputs, trained_gp, nlon, nlat, nlev):
 #     variance, uncer, d = trained_gp.predict(mogp_inputs)
 #     T_mean = mogp_inputs[:, 4:12]
@@ -157,23 +209,38 @@ def data_prep(data, oro, ls, nlon, nlat) -> np.ndarray:
 
 #     return resampled_T, resampled_Q
 
+
 def mogp_prediction(mogp_inputs, trained_gp, nlon, nlat, nlev):
+    """Makes a prediction with the MOGP model.
+    
+    Args:
+        mogp_inputs (np.ndarray): The input data for making a prediction
+        trained_gp (mogp_emulator model): The trained MOGP model
+        nlon (int): The number of longitudes
+        nlat (int): The number of latitudes
+        nlev (int): The number of levels
+    """
     variance, uncer, d = trained_gp.predict(mogp_inputs)
     print("Prediction")
 
+    # Extract current atmopsheric state
     T_mean = mogp_inputs[:, 4:12]
     Q_mean = mogp_inputs[:, 12:]
+    # Setup empty arrays for new atmospheric state
     resampled_T = np.empty((nlon*nlat*nlev), dtype = np.float64)
     resampled_Q = np.empty((nlon*nlat*nlev), dtype = np.float64)
     
-    # # Rescale the Specific Humidity
+    # Rescale the Specific Humidity
     variance[8:,:] = variance[8:,:]/1000
     uncer[8:,:] = uncer[8:,:]/1000
 
     low_values_flags = variance < 1e-6  # Where values are low
     variance[low_values_flags] = 0.0
 
+    ##### Generate the new atmospheric state
+    # Generate random Gaussian perturbations of shape (nlon*nlat, nlev)
     draws = np.random.normal(0, 1, np.shape(T_mean))
+    # New atmopsheric state is the current state plus the pertubration times (the variance plus the uncertainty in the variance)
     resampled_T = T_mean.flatten() + draws.flatten() * (variance[:8,:].T.flatten() + uncer[:8,:].T.flatten())
     resampled_Q = Q_mean.flatten() + draws[:,:5].flatten() * (variance[8:,:].T.flatten() + uncer[8:,:].T.flatten())
 
@@ -185,7 +252,6 @@ def mogp_prediction(mogp_inputs, trained_gp, nlon, nlat, nlev):
     # print(f"Max std T = {np.amax(variance[:8,:])} and Q = {np.amax(variance[8:,:])}")
 
     return resampled_T, resampled_Q
-
 
 
 def main(HYBRID_data_root):
@@ -253,5 +319,3 @@ def main(HYBRID_data_root):
 
 if __name__ == '__main__':
     main(sys.argv[1])
-
-
